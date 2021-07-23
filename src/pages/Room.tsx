beffe8f3e34c9742fda2daf4cee25b64c766724c
Wesley Westelley <wesleywestelley@gmail.com>
2021-07-23 17:43:22 -0300

    :wrench: feat: Added toaster error and send asks.
    new features how sending asks and a alert in toast error.

--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -1,16 +1,103 @@
+import { useAuth } from '../hooks/useAuth'
+import { useState, FormEvent, useEffect } from 'react'
 import logoImg from '../assets/images/logo.svg'
 import { RoomCode } from '../components/RoomCode'
 import { Button } from '../components/Button'
-import {useParams} from 'react-router-dom'
+import { useParams } from 'react-router-dom'
 import '../styles/room.scss'
+import toast, { Toaster } from 'react-hot-toast';
+import { database } from '../services/firebase'
+
+const notify = () => toast.error('The users needs be login to send asks');
+
+
+type FirebaseQuestions = Record<string, {
+    author: {
+        name: string,
+        avatar: string
+    }
+    content: string
+    isAnswered: boolean
+    isHighLighted: boolean
+}>
+
+type Question = {
+    id: string
+    author: {
+        name: string,
+        avatar: string
+    }
+    content: string
+    isAnswered: boolean
+    isHighLighted: boolean
+}
 
 type RoomParams = {
     id: string
 }
 
 export function Room() {
-
+    const { user } = useAuth()
     const params = useParams<RoomParams>()
+    const roomId = params.id
+    const [newQuestion, setNewQuestion] = useState('')
+    const [questions, setQuestions] = useState<Question[]>([])
+    const [title, setTitle] = useState('')
+
+
+    useEffect(() => {
+
+        const roomRef = database.ref(`rooms/${roomId}`)
+
+        roomRef.on('value', room => {
+            const databaseRoom = room.val()
+            const firebaseQuestions: FirebaseQuestions = databaseRoom.questions
+
+            const parsedQuestion = Object.entries(firebaseQuestions).map(([key, value]) => {
+                return {
+                    id: key,
+                    content: value.content,
+                    author: value.author,
+                    isHighLighted: value.isHighLighted,
+                    isAnswered: value.isAnswered
+                }
+            })
+
+            setTitle(databaseRoom.title)
+            setQuestions(parsedQuestion)
+
+        })
+    }, [roomId])
+
+
+    async function handleSendQuestion(event: FormEvent) {
+        event.preventDefault()
+
+        if (newQuestion.trim() === '') {
+            return;
+        }
+
+        if (!user) {
+            notify()
+        }
+
+
+        const question = {
+            content: newQuestion,
+            author: {
+                name: user?.name,
+                avatar: user?.avatar
+            },
+            isHighLighted: false,
+            isAnswered: false
+
+        }
+
+        await database.ref(`rooms/${roomId}/questions`).push(question)
+
+
+        setNewQuestion('')
+    }
 
     return (
         <div id="page-room">
@@ -18,24 +105,46 @@ export function Room() {
             <header>
                 <div className="content">
                     <img src={logoImg} alt="" />
-                    <RoomCode code={params.id}></RoomCode>
+                    <RoomCode code={roomId}></RoomCode>
                 </div>
             </header>
 
             <main>
                 <div className="room-title">
-                    <h1>Sala React</h1>
-                    <span>4 perguntas</span>
+                    <h1>Sala {title}</h1>
+                    {questions.length > 0 && <span>{questions.length} pergunta(s)</span>}
                 </div>
+                <form action="" onSubmit={handleSendQuestion}>
 
-                <form action="">
+                    <textarea placeholder='O que você quer perguntar?' onChange={event => setNewQuestion(event.target.value)} value={newQuestion}></textarea>
 
-                    <textarea placeholder='O que você quer perguntar?'></textarea>
                     <div className="form-footer">
-                        <span>Para enviar uma pergunta, <button>faça seu login</button></span>
-                        <Button type='submit'>Enviar pergunta</Button>
+
+                        {
+                            user ? (
+                                <div className='user-info'>
+                                    <img src={user.avatar} alt={user.name} />
+                                    <span>{user.name}</span>
+                                </div>
+
+                            ) : (<span>Para enviar uma pergunta, <button>faça seu login</button></span>)
+                        }
+
+
+                        <Button type='submit' disabled={!user}>Enviar pergunta</Button>
+                        <Toaster toastOptions={{
+                            className: '',
+                            style: {
+                                border: '1px solid black',
+                                padding: '7px',
+                                color: '#FF0000'
+                            },
+                            position: "top-right"
+                        }}></Toaster>
                     </div>
                 </form>
+
+                {JSON.stringify(questions)}
             </main>
         </div>
     )
