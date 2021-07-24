41a5d1743bb720bbda5ce557cf64ba07844aa649
Wesley Westelley <wesleywestelley@gmail.com>
2021-07-24 16:00:05 -0300

    :wrench: feat: new route added
    added new route to room admin

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { Home } from './pages/Home'
 import { NewRoom } from './pages/NewRoom'
 import { Room } from './pages/Room'
 import { AuthContextProvider } from './contexts/AuthContext'
+import { AdminRoom } from './pages/AdminRoom'
 
 
 
@@ -17,6 +18,7 @@ function App() {
             <Route path='/' exact component={Home}></Route>
             <Route path='/rooms/new' exact component={NewRoom}></Route>
             <Route path='/rooms/:id' component={Room}></Route>
+            <Route path='/admin/rooms/:id' component={AdminRoom}></Route>
           </Switch>
 
         </AuthContextProvider>
